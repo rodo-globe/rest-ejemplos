69f934d7eafbfa274ed747153a0dadcf6762edc7
Rodolfo Vazquez <rodolfo@globe-software.com>
2018-05-15 13:01:34 -0300

    Practico 1

---
+++ src/main/java/uy/org/cjpb/demo/dto/OrderDto.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.org.cjpb.demo.dto;
+
+import java.math.BigDecimal;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author rodo
+ */
+@XmlRootElement
+public class OrderDto {
+    
+    private int id;
+    private String customerName;
+    private BigDecimal amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/uy/org/cjpb/demo/service/rest/OrderRest.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.org.cjpb.demo.service.rest;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
+import uy.org.cjpb.demo.dto.OrderDto;
+
+/**
+ *
+ * @author rodo
+ */
+
+@Path("/orders")
+public class OrderRest {
+    
+    List<OrderDto> orderList = new ArrayList<>();
+    
+    public OrderRest(){
+    
+        OrderDto dto = new OrderDto();
+        dto.setAmount(new BigDecimal(100));
+        dto.setId(1);
+        dto.setCustomerName("Juan Perez");
+       
+        orderList.add(dto);
+        
+        OrderDto dto2 = new OrderDto();
+        dto2.setAmount(new BigDecimal(200));
+        dto2.setId(2);
+        dto2.setCustomerName("Maria Lopez");
+        
+        orderList.add(dto2); 
+        
+        OrderDto dto3 = new OrderDto();
+        dto3.setAmount(new BigDecimal(200));
+        dto3.setId(3);
+        dto3.setCustomerName("Laura Lopez");
+        
+        orderList.add(dto3); 
+    }
+        
+    @GET
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public OrderDto getOrder(@PathParam("id") int id){
+        
+        Logger.getLogger(this.getClass(), "ejecutó getOrder( " + id + " )");
+        
+        return orderList.stream()
+                .filter( o -> o.getId() == id)
+                .findFirst().get();
+    }
+    
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<OrderDto> finByParams(@QueryParam("name") String name){
+    
+        Logger.getLogger(this.getClass(), "ejecutó findByParams( " +name + " )");
+         
+        if(name == null || name.isEmpty()){
+            return orderList;
+        }else{
+            return orderList.stream()
+                .filter(o -> o.getCustomerName().equalsIgnoreCase(name))
+                .collect(Collectors.toList());
+        }
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void update(@PathParam("id") int id, OrderDto order){
+        
+         Logger.getLogger(this.getClass(), "ejecutó update "); 
+    }
+    
+    @DELETE
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void delete(@PathParam("id") int id){
+        
+         Logger.getLogger(this.getClass(), "ejecutó delete ");     
+    }
+    
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public OrderDto insert(OrderDto order){
+        
+         Logger.getLogger(this.getClass(), "ejecutó delete ");    
+         order.setId(999);
+         return order;
+    }    
+}
+
